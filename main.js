384c62ab756995f95d3b2d9cff0c9adf72592764
bushongTech <132151423+bushongTech@users.noreply.github.com>
2023-08-05 20:00:49 -0500

    forgot the function lol

--- main.js
+++ main.js
@@ -93,6 +93,22 @@ function connectToTelemetryService() {
         console.log('Telemetry connection closed');
     });
 }
+function spawnPythonProcess() {
+    // Modify the path as needed
+    const pythonProcess = spawn('python', ['./path_to_your_script/Buttercream.py']);
+
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
 
 app.whenReady().then(() => {
     spawnPythonProcess();
